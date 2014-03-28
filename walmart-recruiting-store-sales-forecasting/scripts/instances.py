a93be70be3435117d304f93387619e325ca5b0e4
LittleDing <dingweijie3000@qq.com>
2014-03-28 21:10:29 +0800

    fill nan values, wmae=4946

--- walmart-recruiting-store-sales-forecasting/scripts/instances.py
+++ walmart-recruiting-store-sales-forecasting/scripts/instances.py
@@ -1,6 +1,7 @@
-import os,time
+import os,time,logging
 import pandas as pd
 import numpy as np
+import scipy.sparse
 from sklearn.feature_extraction import DictVectorizer
 
 import utils,decorators
@@ -9,7 +10,8 @@ import utils,decorators
 def basic_features(base):
 	base = pd.read_csv(os.path.join(utils.DATA_DIR,base))
 	feature = pd.read_csv(os.path.join(utils.DATA_DIR,'features.csv'))
-	return pd.merge(base,feature,on=['Store','Date','IsHoliday'])
+	ans = pd.merge(base,feature,on=['Store','Date','IsHoliday'])
+	return ans
 
 def feature_001(feature,*args):
 	'''
@@ -18,13 +20,25 @@ def feature_001(feature,*args):
 	'''
 	return feature[['Store','Dept','Date','IsHoliday','Temperature', 'Fuel_Price', 'MarkDown1', 'MarkDown2', 'MarkDown3', 'MarkDown4', 'MarkDown5', 'CPI', 'Unemployment']]
 
+def feature_001f(feature):
+	'''
+	@return id => (FeatureFillNa) where nan is filled with median in Store
+	'''
+	basic = pd.read_csv(os.path.join(utils.DATA_DIR,'features.csv'))
+	medians = basic.groupby(['Store']).median()
+	stores = set(basic['Store'])
+	for st in stores :
+		basic[basic['Store']==st] = basic[basic['Store']==st].fillna(medians.loc[st])
+	IDS = feature[['Store','Dept','Date','IsHoliday']]
+	return pd.merge(IDS,basic,on=['Store','Date','IsHoliday'])
+
 def encoding(xs,_type='1-vs-all'):
 	'''
 	@param[in] xs iterable of value
 	@return  DataFrame
 	'''
 	def _1_vs_all(x):
-		v = DictVectorizer(sparse=False)
+		v = DictVectorizer()
 		x = map(lambda k:{k:1},x)
 		return v.fit_transform(x)
 	return {
@@ -37,12 +51,13 @@ def date_mapping_001(f,t):
 	dates = map(lambda x:x.timetuple(),utils.get_periods(f,t))
 	week_of_year = map(lambda x:(x.tm_yday-1)/7,dates)
 	week_of_month = map(lambda x:(x.tm_mday-1)/7,dates)
-	months = map(lambda x:x.tm_mon,dates)
+	months = map(lambda x:x.tm_mon-1,dates)
 	names = map(lambda x:time.strftime('%Y-%m-%d',x),dates)
 	return pd.DataFrame.from_dict({'Date':names,'WeekMonth':week_of_month,'WeekYear':week_of_year,'Month':months});
 
 @decorators.disk_cached(utils.CACHE_DIR+'/date_mapping_002')
 def date_mapping_002(f,t):
+	'''@return Dataframe sparse encoding of date_mapping_001'''
 	mapping = date_mapping_001(f,t)
 	df = mapping[['Date']]
 	for key in filter(lambda f:f!='Date',mapping):
@@ -52,6 +67,18 @@ def date_mapping_002(f,t):
 		df = pd.merge(df,buff,left_index=True,right_index=True)
 	return df
 
+@decorators.disk_cached(utils.CACHE_DIR+'/date_mapping_003')
+def date_mapping_003(f,t):
+	'''@return DataFrame['DateString','week since f','month since f','season since f','year since f']  '''
+	days 	= utils.get_periods(f,t)
+	weeks 	= map(lambda x:(x-days[0]).days/7,days)
+	months 	= map(lambda x:(x-days[0]).days/30,days)
+	seasons = map(lambda x:(x-days[0]).days/90,days)
+	years	= map(lambda x:(x-days[0]).days/365,days)
+	dates = map(lambda x:x.timetuple(),days)
+	names = map(lambda x:time.strftime('%Y-%m-%d',x),dates)
+	return pd.DataFrame.from_dict({'Date':names,'WeekA':weeks,'MonthA':months,'SeasonA':seasons,'YearA':years});
+
 def feature_002(feature):
 	'''
 	@return id => (month,week)
@@ -59,6 +86,10 @@ def feature_002(feature):
 	IDS = feature[['Store','Dept','Date','IsHoliday']]
 	mapping = date_mapping_002('2010-01-01','2014-12-31')
 	return pd.merge(IDS,mapping)
+def feature_002s(feature):
+	IDS = feature[['Store','Dept','Date','IsHoliday']]
+	mapping = date_mapping_001('2010-01-01','2014-12-31')
+	return pd.merge(IDS,mapping),True,{'WeekMonth':5,'WeekYear':53,'Month':12}
 
 @decorators.disk_cached(utils.CACHE_DIR+'/id_mapping_001')
 def id_mapping_001(name,max_id):
@@ -90,6 +121,27 @@ def id_mapping_002(names,max_ids):
 		ans[name] = [ j for r in xrange(rept) for j in xrange(max_ids[i]) for b in xrange(base) ]
 	return ans	
 
+def id_mapping_003(feature,names,max_ids):
+	''' 
+	multi-dimension id mapping of sparse feature return format 
+	@param[in] feature DataFrame object contains ['Store','Dept','Date','IsHoliday'] and other feature columns
+	@param[in] names [ [column_name_need_to_be_mapped ] ] 
+	@param[in] max_ids [ [ range_of_mapping_column ] ]
+	'''
+	ans = feature[['Store','Dept','Date','IsHoliday']]
+	dimensions = {}
+	for i in range(len(names)):
+		name = names[i]
+		num_id = max_ids[i]
+
+		max_id = reduce((lambda a,b:a*b),num_id,1)
+		colName = '-'.join(name)
+		dimensions[colName] = max_id
+		ans[colName] = 1
+		for i,n in enumerate(name) :
+			base = reduce((lambda a,b:a*b),num_id[i+1:],1)
+	return ans,True,dimensions
+
 def feature_003(feature):
 	'''
 	@return id => (storeid,deptid)
@@ -100,6 +152,12 @@ def feature_003(feature):
 	ans = pd.merge(IDS,stores)
 	ans = pd.merge(ans,depts)
 	return ans
+def feature_003s(feature):
+	ans = feature[['Store','Dept','Date','IsHoliday']]
+	ans['StoreID'] = feature['Store']
+	ans['DeptID'] = feature['Dept']
+	return ans,True,{'StoreID':50,'DeptID':100}
+
 def feature_004(feature):
 	'''
 	@return id => (deptid_storeid)
@@ -108,12 +166,76 @@ def feature_004(feature):
 	f = id_mapping_002(['Store','Dept'],[50,100])
 	ans = pd.merge(IDS,f)
 	return ans
+def feature_004s(feature):
+	return id_mapping_003(feature,[['Store','Dept']],[[50,100]] )
 
-def feature_100(feature,keys,values,periods):
+def feature_005(feature):
 	'''
-	@return { (keys+date) : [ sum(value1 in period1),sum(value1 in period2),sum(value1 in period3)...] }
+	@return id => (deptid_IsHoliday,storeid_IsHoliday)
 	'''
-	pass
+	IDS = feature[['Store','Dept','Date','IsHoliday']]
+	stores = id_mapping_002(['Store','IsHoliday'],[50,2])
+	depts  = id_mapping_002(['Dept','IsHoliday'],[100,2])
+	ans = pd.merge(IDS,stores)
+	ans = pd.merge(ans,depts)
+	return ans
+def feature_005s(feature):
+	return id_mapping_003(feature,[['Store','IsHoliday'],['Dept','IsHoliday']],[[50,2],[100,2]])
+def feature_005Ss(feature):
+	return id_mapping_003(feature,[['Store','IsHoliday']],[[50,2]])
+
+def feature_006s(feature):
+	'''@return id => (deptid_week,storeid_week) '''
+	IDS = feature[['Store','Dept','Date','IsHoliday']]
+	mapping = date_mapping_001('2010-01-01','2014-12-31')
+	ans = pd.merge(IDS,mapping)   #,True,{'WeekMonth':5,'WeekYear':53,'Month':12}
+	return id_mapping_003(ans,[['Store','WeekYear'],['Dept','WeekYear']],[[50,53],[100,53]])
+def feature_006Ss(feature):
+	'''@return id => (deptid_week,storeid_week) '''
+	IDS = feature[['Store','Dept','Date','IsHoliday']]
+	mapping = date_mapping_001('2010-01-01','2014-12-31')
+	ans = pd.merge(IDS,mapping)   #,True,{'WeekMonth':5,'WeekYear':53,'Month':12}
+	return id_mapping_003(ans,[['Store','WeekYear']],[[50,53]])
+
+def feature_007s(feature):
+	'''@return id => (dept_markdownXis0,store_markdownXis0) '''
+	IDS = feature[['Store','Dept','Date','IsHoliday']]
+	mappings = []
+	idxs = []
+	for i in range(1,6):
+		key = 'MarkDown%d_Nan'%(i)
+		IDS[key] = feature['MarkDown%d'%i]==np.nan
+		mappings.append(['Store',key])
+		idxs.append([50,2])
+		mappings.append(['Dept',key])
+		idxs.append([100,2])
+	return id_mapping_003(IDS,mappings,idxs)
+
+def feature_008s(feature):
+	'''
+	discrete continue feature with median and std, cut into 20 buckets, std/2 per bucket
+	@return id => discreted 'Temperature', 'Fuel_Price', 'MarkDown1', 'MarkDown2', 'MarkDown3', 'MarkDown4', 'MarkDown5', 'CPI', 'Unemployment' 
+	'''
+	keys = ['Temperature', 'Fuel_Price', 'MarkDown1', 'MarkDown2', 'MarkDown3', 'MarkDown4', 'MarkDown5', 'CPI', 'Unemployment']
+	# compute from feature.csv
+	medians = [60.71,3.51,4743.58,364.57,36.26,1176.42,2727.14,182.76,7.81]
+	stds = [18.68,0.43,9262.75,8793.58,11276.46,6792.33,13086.69,39.74,1.88]
+	dimensions = { '%sDiscreted'%(key):22 for key in keys }
+	ans = feature[['Store','Dept','Date','IsHoliday']]
+	for i,key in enumerate(keys) :
+		name = '%sDiscreted'%(key)
+		ans[name] = (feature[key]-medians[i])*2/stds[i]+10
+		ans[name] = ans[name].map(lambda x:min(21,max(1,x)))
+		ans[name][feature[key]==np.nan] = 0
+	return ans,True,dimensions
+
+def feature_009s(feature):
+	'''
+	@return id => (week,month,season,year) since 2010-01-01  
+	'''
+	IDS 	= feature[['Store','Dept','Date','IsHoliday']]
+	mapping 	= date_mapping_003('2010-01-01','2014-12-31')
+	return pd.merge(IDS,mapping),True,{'WeekA':55*5,'MonthA':13*5,'SeasonA':5*5,'YearA':2*5}
 
 def make_instance(base,versions=[]):
 	'''
@@ -133,6 +255,61 @@ def make_instance(base,versions=[]):
 	W = np.array(ans['IsHoliday']*4 + 1)
 	ID = ans[['Store','Dept','Date']]
 	IDString = map(lambda x:'_'.join(map(str,x)),np.array(ID))
+	return X,Y,W,ID,IDString
+
+@decorators.disk_cached(utils.CACHE_DIR+'/sparse_features')
+def make_sparse_instance(base,versions=[]):
+	'''
+	@param[in] base "source file name in DATA_DIR"
+	@param[in] versions [ (feature_versions,feature_args) ]
+		the specific feature factory is func(*args) => (featureDataFrame,sparse,dimensions)
+		dimensions => { columnName : columnDimension  }
+	@return X,Y,W,ID,IDString
+	'''
+	eps = 1e-10
+	feature = basic_features(base)
+	IDNames = ['Store','Dept','Date','IsHoliday']
+	if 'Weekly_Sales' in feature : IDNames.append('Weekly_Sales')
+	
+	# collect X
+
+	X = feature[IDNames]
+	sparseColumns = {}
+	for args in versions:
+		v = args[0]
+		sparse = False
+		f = globals()['feature_%s'%(v)](feature,*args[1:])
+		if type(f) == tuple and len(f)<=3 :
+			sparse = f[1]
+			if sparse :
+				sparseColumns.update(f[2])
+			f = f[0]
+		X = pd.merge(X,f,on=['Store','Dept','Date','IsHoliday'])
+
+	# basic infos
+	IDNames = ['Store','Dept','Date']
+	ID = X[IDNames]
+	IDString = map(lambda x:'_'.join(map(str,x)),np.array(ID))
+	W = np.array(X['IsHoliday']*4 + 1)
+	Y = np.array(X['Weekly_Sales']) if 'Weekly_Sales' in X else np.zeros(len(X))
+
+	# transform sparse features
+	initColumns = [ '%s_%d'%(n,i) for n,c in sparseColumns.items() for i in range(c) ]
+	initColumns += [ c for c in X.columns if c not in sparseColumns and c not in IDNames and c!='Weekly_Sales' ]
+	columnMapping = { c:i for i,c in enumerate(initColumns) }
+	featureColumns = [ c for c in X.columns if c not in IDNames and c!='Weekly_Sales' ]
+	logging.info('#featuers=%d'%(len(columnMapping)))
+	X = X[featureColumns].fillna(0)
+	data,row,col = [],[],[]
+	for i in range(len(Y)):
+		ins = X.loc[i]
+		items = [ ('%s_%d'%(c,ins[c]),1) if c in sparseColumns else (c,ins[c]) for c in featureColumns ]
+		data += [ v for k,v in items ]
+		row  += [i]*len(items)
+		col  += [ columnMapping[k]for k,v in items ]
+		if i%10000==0 :
+			logging.info('%d lines generated'%i)
+	X = scipy.sparse.csr_matrix((data,(row,col)))
 	return X,Y,W,ID,IDString
 
 def make_svd_feature_input(outprefix,base,versions=[]):
--- walmart-recruiting-store-sales-forecasting/scripts/solutions.py
+++ walmart-recruiting-store-sales-forecasting/scripts/solutions.py
@@ -1,43 +1,131 @@
 from optparse import OptionParser
-import random,logging,traceback,datetime,os
+import random,logging,traceback,datetime,os,collections
 import sklearn
 import sklearn.linear_model
 import sklearn.svm
+import sklearn.ensemble
 import pandas as pd
 
 import utils,instances
 
 class Predictor():
-	def __init__(self,ids):
-		pass
+	'''
+	build a predictor for every single id in ids
+	'''
+	def __init__(self,ids,modelFactory,supportW=True,supportSparse=True):
+		self.ids = ids
+		self.models = {}
+		self.modelFactory = modelFactory
+		self.supportW = supportW
+		self.supportSparse = supportSparse
 	def split(self,ID):
-		pass
+		index = collections.defaultdict(list)
+		for i in range(len(ID)):
+			key = '-'.join(map(str,ID.loc[i][self.ids]))
+			index[key].append(i)
+		return index
 	def fit(self,X,Y,W,ID):
-		pass
+		index = self.split(ID)
+		for key in sorted(index.keys()):
+			idx = index[key]
+			model = self.modelFactory()    #sklearn.linear_model.LinearRegression()
+			if not self.supportW:
+				idx = [ i for i in idx for j in range(int(W[i])) ]
+			x,y,w = X[idx],Y[idx],W[idx]
+			if not self.supportSparse:
+				x = x.toarray()
+			if self.supportW :
+				model.fit(x,y,w)
+			else :
+				model.fit(x,y)
+			self.models[key] = model
+			logging.info('model %s fit with #%d ins'%(key,len(idx)))
+		return self
 	def predict(self,X,ID):
-		pass
+		index = self.split(ID)
+		Y = [0]*len(ID)
+		for key in sorted(index.keys()):
+			idx = index[key]
+			if key not in self.models : 
+				continue
+			model = self.models[key]
+			x = X[idx]
+			if not self.supportSparse:
+				x = x.toarray()
+			y = model.predict(x)
+			for i in xrange(len(idx)):
+				Y[idx[i]] = y[i]
+			logging.info('#%d of id %s predited'%(len(idx),key))
+		return Y
 
-
-def solution(train_path,test_path,modelFactory=sklearn.linear_model.LinearRegression,feature=[],version='current'):
-	train_x,train_y,train_w,train_ID,train_IDString = instances.make_instance(train_path,feature)
-	
+def solution(train_path,test_path,
+		modelFactory=sklearn.linear_model.LinearRegression,
+		modelNeedID=False,
+		featureFactory=instances.make_instance,
+		feature=[],version='current'):
+	train_x,train_y,train_w,train_ID,train_IDString = featureFactory(train_path,feature)
+	logging.info('training data loaded!')
 	model = modelFactory()
-	model = model.fit(train_x,train_y,train_w)
-	yy = model.predict(train_x)
+	if modelNeedID :
+		model = model.fit(train_x,train_y,train_w,train_ID)
+		logging.info('model fit!')
+		yy = model.predict(train_x,train_ID)
+	else :
+		model = model.fit(train_x,train_y,train_w)
+		logging.info('model fit!')
+		yy = model.predict(train_x)
 	logging.info('wmae on training set : %s'%(utils.wmae(train_y,yy,train_w) ))
 	
-	test_x,test_y,test_w,test_ID,test_IDString = instances.make_instance(test_path,feature)
-	yy = model.predict(test_x)
+	test_x,test_y,test_w,test_ID,test_IDString = featureFactory(test_path,feature)
+	logging.info('testing data loaded!')
+	if modelNeedID :
+		yy = model.predict(test_x,test_ID)
+	else :
+		yy = model.predict(test_x)
 	ans = pd.DataFrame.from_dict({'Id':test_IDString,'Weekly_Sales':yy})
 	ans.to_csv(os.path.join(utils.ANS_DIR,version+'.txt'),index=False)
 
 solutions = {
-	'current' : {
-		'modelFactory' : ('sklearn.svm.SVR',{'kernel':'poly','C':100}), 
+	'20140314' : {
+		'modelFactory' : ('sklearn.linear_model.LinearRegression',{}),
+		'modelNeedID' : False,
 		'train_path' : 'train.csv',
 		'test_path' : 'test.csv',
-		'feature' : [('001',),('002',),('003',)],
-	}
+		'feature' : [('001',),('002s',),('003s',),('006s',)],
+		'featureFactory' : 'make_sparse_instance'
+	},
+	'LR' : {
+		'modelFactory' : ('sklearn.linear_model.LinearRegression',{}),
+		'modelNeedID' : False,
+		'train_path' : 'train.csv',
+		'test_path' : 'test.csv',
+		'feature' : [('001f',),('002s',),('003s',),('004s',),
+				('005s',),('006s',),
+				('007s',),('008s',),
+				('009s',),
+		],
+		'featureFactory' : 'make_sparse_instance'
+	},
+	'current' : {
+		'modelFactory' : (
+				'Predictor',
+				{
+					'ids' : ['Dept','Store'],
+					'modelFactory' : sklearn.linear_model.LinearRegression,
+					#'modelFactory' :(lambda :sklearn.ensemble.GradientBoostingRegressor(n_estimators=100,max_depth=5)),
+					'supportW' : True,
+					'supportSparse' : True,
+				}
+		),
+		'modelNeedID' : True,
+		'train_path' : 'train.csv',
+		'test_path' : 'test.csv',
+		'feature' : [('001f',),('002s',),('003s',),
+				#('005Ss',),('006Ss',),
+				('008s',),
+		],
+		'featureFactory' : 'make_sparse_instance'
+	},
 }
 
 def get_object(base,names):
@@ -55,11 +143,12 @@ def run_solution(version='current'):
 	karg = solutions[version]
 	karg['version'] = version
 	karg['modelFactory'] = make_model_factory(*karg['modelFactory'])
+	karg['featureFactory'] = instances.__dict__[ karg['featureFactory'] ]
 	solution(**karg)
 
 def main():
 	parser = OptionParser()
-	parser.add_option('', '--job', dest='job',help='jobname');
+	parser.add_option('', '--job', dest='job',help='jobname',default='run_solution');
 	parser.add_option('', '--seed', dest='seed',type=int,default=123,help='random seed');
 	parser.add_option('', '--options', dest='options',type=int,default=0
 		,help='if set to 1, function will call with optparser\'s option as first argument');
